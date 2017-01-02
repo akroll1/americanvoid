b5f28c0a52dd518016c21cb70e45f33176bd7c78
Andrew Kroll <andrew.kroll@yahoo.com>
2017-01-01 20:40:07 -0500

    updating site considerably

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,13 +1,33 @@
-var keystone = require('keystone');
+var keystone = require('keystone'),
+		async = require('async');
 
 exports = module.exports = function(req, res) {
-	
+
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
+	locals.data = {
+		artilces: []
+	}
+
+	view.on('init', function(next){
+		keystone.list('Post').model.find()
+			.where('state', 'published')
+	    .populate('author')
+	    .sort('-publishedDate')
+	    .limit(6)
+	    .exec(function(err, posts) {
+				if (err){
+					return 'Here is err ', err;
+				}
+				locals.data.articles = posts;
+				console.log('here is locals.data.articles.length ', locals.data.articles.length);
+				next(err);
+    	});
+	});
 
 	// Render the view
 	view.render('index');
